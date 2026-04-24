040451a4eb7d57a727a678532d4fcb1426e1e91b
Daniel Götten <daniel.goetten@gmail.com>
2026-04-24 16:49:35 +0200

    Added missing module infos (#12)

---
+++ persistence/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/**
+ * slact persistence extension.
+ */
+module de.dangoe.concurrent.slact.persistence {
+  exports de.dangoe.concurrent.slact.persistence;
+  exports de.dangoe.concurrent.slact.persistence.exception;
+  requires transitive de.dangoe.concurrent.slact;
+  requires org.jetbrains.annotations;
+}
